81adef6701d118865919dca942eee8ab81d16ec7
Saket Gulhane <saket.gulhane291@gmail.com>
2020-03-02 22:26:18 +0530

    final2 deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function search() {
   var req = new XMLHttpRequest();
   //   var payload = { location: null, temperature: null, humidity: null };
   var urlSend =
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
     document.getElementById("city").value +
     "&units=metric&appid=7a152efb80d0ac3843a672f527bfa1a3";
   req.open("GET", urlSend, true);
@@ -23,7 +23,7 @@ function search() {
       var des = response.weather[0].description;
       var img = response.weather[0].icon;
       //image
-      var imageURL = "http://openweathermap.org/img/wn/" + img + "@2x.png";
+      var imageURL = "https://openweathermap.org/img/wn/" + img + "@2x.png";
 
       document.getElementById("location").textContent = city;
       document.getElementById("temp").textContent = temp;
@@ -31,7 +31,7 @@ function search() {
       var img = document.createElement("img");
       img.src = imageURL;
 
-      document.getElementById("image").appendChild(img); 
+      document.getElementById("image").appendChild(img);
       document.getElementById("des").textContent = des;
     } else {
       console.log("Error in network request: " + req.statusText);
